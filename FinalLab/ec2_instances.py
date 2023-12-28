d4c4cdc2cae332fcc1a39a21f9440e878ad4c201
Danilo Vaz Marcolino Alves <39676558+danilovazm@users.noreply.github.com>
2023-12-28 13:46:01 -0500

    instances creation

--- FinalLab/ec2_instances.py
+++ FinalLab/ec2_instances.py
@@ -1,11 +1,12 @@
 import boto3
 import subprocess, pprint, time
 import util
+import requests
 
-def EC2_instances(ec2_client, sgId):
+def EC2_instances(ec2_client, sgId, proxySg):
     instanceIds = []
     amiId = 'ami-06aa3f7caf3a30282' #Os image that will be used in the vm
-    ClusterPrivateIps = ['172.31.2.3', '172.31.2.4', '172.31.2.5', '172.31.2.2']
+    ClusterPrivateIps = ['172.31.2.3', '172.31.2.4', '172.31.2.5', '172.31.2.2', '172.31.2.6', '172.31.2.7']
     #subnet = util.fetch_subnet(ec2_client)[0]
 
     # creates key pair and saves the secret key locally
@@ -21,14 +22,33 @@ def EC2_instances(ec2_client, sgId):
         with open("Worker-node2.sh", "r") as file:
             script = file.read()
         return script
-    
-    def launch_Mgm():
-        with open("Mgm-node2.sh", "r") as file:
+
+    def launch_gate():
+        with open("gate.sh", "r") as file:
+            script = file.read()
+        return script
+
+    def launch_proxy():
+        with open("proxy.sh", "r") as file:
             script = file.read()
         return script
     
+    def launch_mgm():
+        with open("Mgm-node2.sh", "r") as file:
+            script = file.read()
+        return script
+    def scripts(role):
+        if role == 'worker':
+            return launch_worker()
+        elif role == 'mgm' :
+            return launch_mgm()
+        elif role == 'gate':
+            return launch_gate()
+        elif role ==  'proxy':
+            return launch_proxy()
+            
     # ec2 instance launcher
-    def launch_instance(KPName, Itype, privateIp,role = 0):
+    def launch_instance(KPName, Itype, privateIp = None, role = 'worker'):
         response = ec2_client.run_instances(
                     PrivateIpAddress=privateIp,
                     SubnetId='subnet-0facea78a1e94b6cd',
@@ -37,42 +57,45 @@ def EC2_instances(ec2_client, sgId):
                     MaxCount=1,
                     InstanceType=Itype,
                     KeyName=KPName,
-                    SecurityGroupIds=[sgId],
-                    UserData=launch_Mgm() if role == 1 else launch_worker() # passing the loaded bash script to the instance
+                    SecurityGroupIds=[proxySg] if role == 'proxy' else [sgId],
+                    UserData=scripts(role) # passing the loaded bash script to the instance
         )
-        '''ec2 = boto3.resource('ec2')
+        ec2 = boto3.resource('ec2')
         id = response['Instances'][0]['InstanceId']
         instance = ec2.Instance(id)
         instance.wait_until_running()
         instance.reload()
         
         pprint.pprint(instance.public_ip_address)
-        return id, instance.public_ip_address'''
-        return id
+        return id, instance.public_ip_address
+        return id, instance.public_ip_address
 
     # launch the diffent set of instances
     def launch_cluster(KPName, type):
         for i in range(3):
-            instanceIds.append(launch_instance(KPName, type, ClusterPrivateIps[i]))
+            instanceIds.append(launch_instance(KPName, type, ClusterPrivateIps[i], 'worker'))
 
     # store ip adress of instances in files
-    def storeIpAddresses(instanceIds):
-        for i in range(4):
-            with open(f"ip{i+1}.txt", "w") as file:
-                file.write(instanceIds[i][1])
+    def storeIpAddresses(instanceId):
+        with open(f"gate.txt", "w") as file:
+            file.write(instanceIds[1])
+
+    def initiate_gatekeeper(ip):
+        requests.post(f'http://{ip}:5000/initiate', data={'ip': ip})
             
 
     # creating key pair and lauching the instances
     KPName = create_key_pair('3rd-assign-key')
-    instanceIds.append(launch_instance(KPName, 't2.micro', ClusterPrivateIps[-1], 1))
+    instanceIds.append(launch_instance(KPName, 'm4.large', ClusterPrivateIps[-3], 'mgm'))     # launching manager
     time.sleep(180)
-    launch_cluster(KPName, 'm4.large')
+    launch_cluster(KPName, 'm4.large')                                                        # launching worker
+    instanceIds.append(launch_instance(KPName, 't2.large', ClusterPrivateIps[-3], 'gate'))    # launching gatekeeper
+    time.sleep(60)
+    storeIpAddresses(instanceIds[4])
+    subprocess.run(['sh', './proxy.sh'])
+    instanceIds.append(launch_instance(KPName, 't2.large', ClusterPrivateIps[-3], 'proxy'))   # launching proxy
 
-    # store ip adresses of workers
-    #storeIpAddresses(instanceIds)
-
-    # run the orchestrator script 
-    #result = subprocess.run(['sh', './orchestrator.sh'])
+    initiate_gatekeeper(instancesIds[-1][1])                                                  # Initiating the gatekeeper with the proxy public ip as the trusted host
     
 
 
